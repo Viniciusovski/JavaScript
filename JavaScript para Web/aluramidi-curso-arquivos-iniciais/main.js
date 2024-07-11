c491e3ddf0a61b886079eb5cf371de0feeaa98e3
Viniciusovski <vini98.2012@gmail.com>
2024-07-10 21:27:08 -0300

    O que é uma função?

--- JavaScript para Web/aluramidi-curso-arquivos-iniciais/main.js
+++ JavaScript para Web/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,6 +1,8 @@
-alert('Olá Mundo');
+function tocaSomPom(){
+    // Executar som
+    document.querySelector('#som_tecla_pom').play();
+}
 
 // Utliza o document para acessar o documento html
 document.querySelector('.tecla_pom');
-// Executar som
-document.querySelector('#som_tecla_pom').play();
+
