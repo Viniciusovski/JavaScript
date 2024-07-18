50245a4323a564ac2c6ff027f8f4a9cfcd2a8d48
Viniciusovski <vini98.2012@gmail.com>
2024-07-17 21:06:59 -0300

    Referencia

--- JavaScript para Web/aluramidi-curso-arquivos-iniciais/main.js
+++ JavaScript para Web/aluramidi-curso-arquivos-iniciais/main.js
@@ -3,12 +3,7 @@ function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
 
-function tocaSomClap(){
-    // Executar som
-    document.querySelector('#som_tecla_clap').play();
-}
-// É preciso tirar os parenteses para fazer a atribuição no onclick
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
 // Lista de classes
 document.querySelectorAll('.tecla');
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
