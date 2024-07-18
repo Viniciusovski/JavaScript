882f58d980e805d6df6a812d2f3b5dbd9ac71caf
Viniciusovski <vini98.2012@gmail.com>
2024-07-17 21:32:46 -0300

    Percorrendo uma lista

--- JavaScript para Web/aluramidi-curso-arquivos-iniciais/main.js
+++ JavaScript para Web/aluramidi-curso-arquivos-iniciais/main.js
@@ -10,3 +10,10 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 listaDeTeclas[0].onclick = tocaSomPom;
 
+while(contador < listaDeTeclas.length){
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
