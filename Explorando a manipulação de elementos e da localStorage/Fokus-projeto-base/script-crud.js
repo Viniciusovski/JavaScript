29a8afc6e0282cc513030ac9afb1e4d6aec11f7a
Viniciusovski <vini98.2012@gmail.com>
2024-08-16 10:48:29 -0300

    Submetendo o formulário

--- Explorando a manipulação de elementos e da localStorage/Fokus-projeto-base/script-crud.js
+++ Explorando a manipulação de elementos e da localStorage/Fokus-projeto-base/script-crud.js
@@ -2,8 +2,26 @@
 
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAddTarefa = document.querySelector('.app__form-add-task')
+const textArea = document.querySelector('.app__form-textarea')
+
+const tarefas = []
 
 btnAdicionarTarefa.addEventListener('click', () => {
-    // mostrar ou ocultar o formulario
+    // mostrar ou ocultar o  formulario
     formAddTarefa.classList.toggle('hidden')
+})
+
+// Evento de submissão do formulário
+formAddTarefa.addEventListener('submit', (event) => {
+    // Prevenir que o comportamento da página seja padrão
+    event.preventDefault();
+    // Objeto tarefa
+    const tarefa = {
+        descricao: textArea.value
+    }
+    // Adicionar objeto tarefa ao array
+    tarefas.push(tarefa)
+
+    // Guarda a lista de tarefas no localStorage
+    localStorage.setItem('tarefas', tarefas)
 })
