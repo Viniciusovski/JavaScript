300f9b8d93c45affde8e5efc192f7e7c2ce0a87b
Viniciusovski <vini98.2012@gmail.com>
2024-08-20 22:24:14 -0300

    Selecionando uma tarefa

--- Explorando a manipulação de elementos e da localStorage/Fokus-projeto-base/script-crud.js
+++ Explorando a manipulação de elementos e da localStorage/Fokus-projeto-base/script-crud.js
@@ -4,6 +4,7 @@ const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAddTarefa = document.querySelector('.app__form-add-task')
 const textArea = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-task-description')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
@@ -51,6 +52,11 @@ function criarElementoTarefa(tarefa) {
     li.append(svg)
     li.append(paragrafo)
     li.append(butao)    
+
+    li.onclick = () => {
+        paragrafoDescricaoTarefa.textContent = paragrafo.textContent
+        li.classList.add('app__section-task-list-item--active')
+    }
     
     return li
 }
